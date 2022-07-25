dde221b8a54d5cd891fed56bd8ab5fd3b0015f47
dalbeee <acapelles@gmail.com>
2022-07-25 12:39:06 +0900

    modify: messageService not use gateway

--- backend/src/message/message.module.ts
+++ backend/src/message/message.module.ts
@@ -1,13 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { PrismaService } from '@src/prisma.service';
-import { SocketIoModule } from '@src/socketio/socketio.module';
 import { MessageReactionService } from './message-reaction.service';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 
 @Module({
-  imports: [forwardRef(() => SocketIoModule)],
   controllers: [MessageController],
   providers: [PrismaService, MessageService, MessageReactionService],
   exports: [MessageReactionService, MessageService],
