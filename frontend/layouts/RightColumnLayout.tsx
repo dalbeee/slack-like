24f6e74019c1ca1e05da1a3628b6cbc58bf97c0d
dalbeee <acapelles@gmail.com>
2022-08-01 02:02:07 +0900

    fix: update ui

--- frontend/layouts/RightColumnLayout.tsx
+++ frontend/layouts/RightColumnLayout.tsx
@@ -1,11 +1,7 @@
 import ThreadViewer from "@/module/app/message/components/ThreadViewer";
 
 const RightColumnLayout = () => {
-  return (
-    <div className="w-5/12 border border-neutral-700">
-      <ThreadViewer />
-    </div>
-  );
+  return <ThreadViewer />;
 };
 
 export default RightColumnLayout;
